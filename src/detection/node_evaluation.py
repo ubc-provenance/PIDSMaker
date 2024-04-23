1b57a895b378cdc2d09761db34065955cdbcf47b
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-04-22 17:33:57 -0700

    add supervised optimal threshold

--- src/detection/node_evaluation.py
+++ src/detection/node_evaluation.py
@@ -1,5 +1,10 @@
 import torch
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import (
+    confusion_matrix,
+    auc,
+    roc_auc_score,
+    roc_curve,
+)
 
 from provnet_utils import *
 from config import *
@@ -23,8 +28,20 @@ def calculate_max_val_loss_threshold(graph_dir, logger):
 
     return thr
 
-def calculate_supervised_best_threshold():
-    pass
+def calculate_supervised_best_threshold(losses, labels):
+    fpr, tpr, thresholds = roc_curve(labels, losses)
+    roc_auc = auc(fpr, tpr)
+
+    # Filter out the points where TPR is less than 0.84
+    valid_indices = np.where(tpr >= 0.84)[0]
+    fpr_valid = fpr[valid_indices]
+    thresholds_valid = thresholds[valid_indices]
+
+    # Find the threshold corresponding to the lowest FPR among valid points
+    optimal_idx = np.argmin(fpr_valid)
+    optimal_threshold = thresholds_valid[optimal_idx]
+
+    return optimal_threshold
 
 def classifier_evaluation(y_test, y_test_pred, logger):
     tn, fp, fn, tp =confusion_matrix(y_test, y_test_pred).ravel()
@@ -59,53 +76,73 @@ def get_ground_truth_nids(cfg):
                 ground_truth_nids.append(int(node_id))
     return ground_truth_nids
 
-def node_evaluation_without_triage(thr, tw_path, logger, cfg):
+def get_edge_index_losses_labels(tw_path, logger, cfg):
     ground_truth_nids = get_ground_truth_nids(cfg)
 
-    logger.info("Loading data...")
-    labels = {}
-    pred_labels ={}
+    logger.info(f"Loading data from {tw_path}...")
+    node_labels = {}
+    edge_index, losses, edge_labels = [], [], []
 
     filelist = listdir_sorted(tw_path)
-    for file in tqdm(sorted(filelist), desc="Initialize the node labels in test set"):
+    for file in tqdm(sorted(filelist), desc="Compute labels"):
         file = os.path.join(tw_path, file)
-        logger.info(f"Loading data from file: {file}")
         with open(file, 'r') as f:
             for line in f:
                 l = line.strip()
                 data = eval(l)
                 srcnode = data['srcnode']
                 dstnode = data['dstnode']
-
                 loss = data['loss']
+                
+                edge_index.append((srcnode, dstnode))
+                losses.append(loss)
 
-                if srcnode not in labels:
-                    labels[srcnode] = 0
-
-                if dstnode not in labels:
-                    labels[dstnode] = 0
-
-                if loss > thr:
-                    pred_labels[srcnode] = 1
+                if cfg.detection.node_evaluation.use_dst_node_loss:
+                    edge_labels.append(srcnode in ground_truth_nids or dstnode in ground_truth_nids)
                 else:
-                    if srcnode not in pred_labels:
-                        pred_labels[srcnode] = 0
+                    edge_labels.append(srcnode in ground_truth_nids)
 
-                if loss > thr:
-                    pred_labels[dstnode] = 1
-                else:
-                    if dstnode not in pred_labels:
-                        pred_labels[dstnode] = 0
+                if srcnode not in node_labels:
+                    node_labels[srcnode] = 0
+                if dstnode not in node_labels and cfg.detection.node_evaluation.use_dst_node_loss:
+                    node_labels[dstnode] = 0
 
-    logger.info(f"Labelling...")
-    for nid in labels:
+    for nid in node_labels:
         if nid in ground_truth_nids:
-            labels[nid] = 1
+            node_labels[nid] = 1
+
+    return edge_index, losses, node_labels, edge_labels
+
+def node_evaluation_without_triage(val_tw_path, tw_path, logger, cfg):
+    edge_index, losses, node_labels, edge_labels = get_edge_index_losses_labels(tw_path, logger, cfg)
+    
+    threshold_method = cfg.detection.node_evaluation.threshold_method
+    if threshold_method == "max_val_loss":
+        thr = calculate_max_val_loss_threshold(val_tw_path, logger)
+    elif threshold_method == "supervised_best_threshold":
+        thr = calculate_supervised_best_threshold(losses, edge_labels)
+    else:
+        raise ValueError(f"Invalid threshold method `{threshold_method}`")
+    
+    pred_labels = {}
+    for (srcnode, dstnode), loss, edge_label in tqdm(zip(edge_index, losses, edge_labels), desc="Edge thresholding"):
+        if loss > thr:
+            pred_labels[srcnode] = 1
+        else:
+            if srcnode not in pred_labels:
+                pred_labels[srcnode] = 0
+
+        if cfg.detection.node_evaluation.use_dst_node_loss:
+            if loss > thr:
+                pred_labels[dstnode] = 1
+            else:
+                if dstnode not in pred_labels:
+                    pred_labels[dstnode] = 0
 
     y_truth = []
     y_pred = []
-    for nid in labels:
-        y_truth.append(labels[nid])
+    for nid in node_labels:
+        y_truth.append(node_labels[nid])
         y_pred.append(pred_labels[nid])
 
     logger.info(f"Start evaluating...")
@@ -116,24 +153,16 @@ def main(cfg):
         name="node_evaluation",
         filename=os.path.join(cfg.detection.node_evaluation._logs_dir, "node_evaluation.log"))
 
-    threshold_method = cfg.detection.node_evaluation.threshold_method
-    if threshold_method == "max_val_loss":
-        test_losses_dir = os.path.join(cfg.detection.gnn_testing._edge_losses_dir, "test")
-        val_losses_dir = os.path.join(cfg.detection.gnn_testing._edge_losses_dir, "val")
-        
-        for model_epoch_dir in listdir_sorted(test_losses_dir):
-            print(f"Evaluation of model {model_epoch_dir}...")
+    test_losses_dir = os.path.join(cfg.detection.gnn_testing._edge_losses_dir, "test")
+    val_losses_dir = os.path.join(cfg.detection.gnn_testing._edge_losses_dir, "val")
+    
+    for model_epoch_dir in listdir_sorted(test_losses_dir):
+        print(f"Evaluation of model {model_epoch_dir}...")
 
-            test_tw_path = os.path.join(test_losses_dir, model_epoch_dir)
-            val_tw_path = os.path.join(val_losses_dir, model_epoch_dir)
+        test_tw_path = os.path.join(test_losses_dir, model_epoch_dir)
+        val_tw_path = os.path.join(val_losses_dir, model_epoch_dir)
 
-            thr = calculate_max_val_loss_threshold(val_tw_path, logger)
-            node_evaluation_without_triage(thr, test_tw_path, logger, cfg)
-
-    elif threshold_method == "supervised_best_threshold":
-        thr = calculate_supervised_best_threshold(valpath)
-    else:
-        raise ValueError(f"Invalid threshold method `{threshold_method}`")
+        node_evaluation_without_triage(val_tw_path, test_tw_path, logger, cfg)
 
 
 if __name__ == "__main__":
