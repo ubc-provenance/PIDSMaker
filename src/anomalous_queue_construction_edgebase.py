f4c6aaccac87f717126af2ae697693b545f65e4d
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-05-01 12:22:31 -0700

    time window queues evaluation part 3

--- src/anomalous_queue_construction_edgebase.py
+++ src/anomalous_queue_construction_edgebase.py
@@ -1,7 +1,6 @@
-import argparse
-import logging
-
 import torch
+import re
+import wandb
 from sklearn.neighbors import LocalOutlierFactor
 
 from provnet_utils import *
@@ -156,14 +155,26 @@ def train_lof_model(cfg):
             embeddings.append(labels_and_embeddings[label])
 
     clf = LocalOutlierFactor(novelty=True, n_neighbors=30).fit(embeddings)
-    torch.save(clf, os.path.join(cfg.detection.tw_evaluation._task_path, "trained_lof.pkl"))
+    torch.save(clf, os.path.join(cfg.detection.queue_evaluation._task_path, "trained_lof.pkl"))
     return clf
 
-def main(cfg):
-    logger = get_logger(
-        name="tw_evaluation",
-        filename=os.path.join(cfg.detection.tw_evaluation._logs_dir, "tw_evaluation.log"))
+def ground_truth_label(test_tw_path):
+    labels = {}
+    for tw in listdir_sorted(test_tw_path):
+        labels[tw] = 0
 
+    attack_tws = cfg.dataset.ground_truth_time_windows
+    for tw in attack_tws:
+        # TODO: this is a workaround, should be replaced with exact event timestamps
+        label_tw_match = [label for label in labels.keys() if tw.startswith(label[:19])]
+        assert len(label_tw_match) > 0, f"Attack time window file not found: {tw}"
+        
+        matched_label_tw = label_tw_match[0]
+        labels[matched_label_tw] = 1
+
+    return labels
+
+def create_queues(cfg):
     node2vec_path = os.path.join(cfg.featurization.embed_nodes._vec_graphs_dir, "nodelabel2vec")
     node2vec_train_val_path = os.path.join(cfg.featurization.embed_nodes._vec_graphs_dir, "nodelabel2vec_val")
     
@@ -191,10 +202,74 @@ def main(cfg):
             val_thr=val_thr
         )
         
-        out_dir = cfg.detection.tw_evaluation._queues_dir
+        out_dir = cfg.detection.queue_evaluation._queues_dir
         os.makedirs(out_dir, exist_ok=True)
         torch.save(queues, os.path.join(out_dir, f"{model_epoch_dir}_queues.pkl"))
 
+def predict_queues(cfg):
+    print(f"threshold 1: {beta_day10}") # TODO: remove
+    print(f"threshold 2: {beta_day11}")
+
+    # Evaluating the testing set
+    test_losses_dir = os.path.join(cfg.detection.gnn_testing._edge_losses_dir, "test")
+    
+    best_precision, best_stats = 0.0, None
+    for model_epoch_dir in listdir_sorted(test_losses_dir):
+        test_tw_path = os.path.join(test_losses_dir, model_epoch_dir)
+        
+        pred_label = {}
+        for tw in listdir_sorted(test_tw_path):
+            pred_label[tw] = 0
+        
+        queues = torch.load(os.path.join(cfg.detection.queue_evaluation._queues_dir, f"{model_epoch_dir}_queues.pkl"))
+        labels = ground_truth_label(test_tw_path)
+
+        detected_queues = []
+        for queue in queues:
+            label = any([labels[hq["name"]] for hq in queue ])
+            anomaly_score = 0
+            for hq in queue:
+                if anomaly_score == 0:
+                    anomaly_score = (anomaly_score + 1) * (hq['loss'] + 1)
+                else:
+                    anomaly_score = (anomaly_score) * (hq['loss'] + 1)
+            print(f"-> queue anomaly score: {anomaly_score:.2f} | {'ATTACK' if label else ''}")
+            
+            if anomaly_score > beta_day10:
+                name_list = []
+                for i in queue:
+                    name_list.append(i['name'])
+                print(f"Anomalous queue: {name_list}")
+                detected_queues.append(name_list)
+                for i in name_list:
+                    pred_label[i] = 1
+                print(f"Anomaly score: {anomaly_score}")
+        
+        out_dir = cfg.detection.queue_evaluation._predicted_queues_dir
+        os.makedirs(out_dir, exist_ok=True)
+        torch.save(detected_queues, os.path.join(out_dir, f"{model_epoch_dir}_predicted_queues.pkl"))
+
+        # Calculate the metrics
+        print("\n********************************* Attack Labels *********************************")
+        y, y_pred = [], []
+        for i in labels:
+            y.append(labels[i])
+            y_pred.append(pred_label[i])
+
+        stats = classifier_evaluation(y, y_pred, y_pred)
+        stats["epoch"] = int(re.findall(r'[+-]?\d*\.?\d+', model_epoch_dir)[0])
+        wandb.log(stats)
+        
+        if precision > best_precision:
+            best_precision = precision
+            best_stats = stats
+        
+    wandb.log(best_stats)
+
+def main(cfg):
+    create_queues(cfg)
+    predict_queues(cfg)
+    
 
 if __name__ == "__main__":
     args = get_runtime_required_args()
