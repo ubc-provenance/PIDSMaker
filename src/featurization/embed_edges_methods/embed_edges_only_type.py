1abb2abc8b68510fbcf31249a003322928b3b0d9
jiang-baoxiang <2402395668@qq.com>
2024-05-29 13:44:01 -0700

    Add embed edges with node type only

--- src/featurization/embed_edges_methods/embed_edges_only_type.py
+++ src/featurization/embed_edges_methods/embed_edges_only_type.py
@@ -1,8 +1,94 @@
+import os
+from provnet_utils import *
+from config import *
 
+import numpy as np
+from tqdm import tqdm
+from torch_geometric.data import *
+import torch
 
+def gen_vectorized_graphs(etype2oh, ntype2oh, split_files, out_dir, logger, cfg):
+    base_dir = cfg.preprocessing.build_graphs._graphs_dir
+    sorted_paths = get_all_files_from_folders(base_dir, split_files)
+
+    for path in tqdm(sorted_paths, desc="Computing edge embeddings"):
+        file = path.split("/")[-1]
+
+        logger.info(f"Processing graph: {file}")
+        graph = torch.load(path)
+
+        sorted_edges = sorted(graph.edges(data=True, keys=True), key=lambda t: t[3]["time"])
+
+        dataset = TemporalData()
+        src = []
+        dst = []
+        msg = []
+        t = []
+        for u, v, k, attr in sorted_edges:
+            src.append(int(u))
+            dst.append(int(v))
+
+            msg.append(torch.cat([
+                ntype2oh[graph.nodes[u]['node_type']],
+                etype2oh[attr["label"]],
+                ntype2oh[graph.nodes[v]['node_type']]
+            ]))
+            t.append(int(attr["time"]))
+
+        dataset.src = torch.tensor(src)
+        dataset.dst = torch.tensor(dst)
+        dataset.t = torch.tensor(t)
+        dataset.msg = torch.vstack(msg)
+        dataset.src = dataset.src.to(torch.long)
+        dataset.dst = dataset.dst.to(torch.long)
+        dataset.msg = dataset.msg.to(torch.float)
+        dataset.t = dataset.t.to(torch.long)
+
+        os.makedirs(out_dir, exist_ok=True)
+        torch.save(dataset, os.path.join(out_dir, f"{file}.TemporalData.simple"))
+
+        logger.info(f'Graph: {file}. Events num: {len(sorted_edges)}. Node num: {len(graph.nodes)}')
 
 def main(cfg):
-    pass
+    logger = get_logger(
+        name="embed_edges_with_node_type_only",
+        filename=os.path.join(cfg.featurization.embed_edges._logs_dir, "embed_edges.log")
+    )
+
+    logger.info("Loading node msg from database...")
+
+    etype2onehot = gen_relation_onehot(rel2id=rel2id)
+    ntype2onehot = gen_relation_onehot(rel2id=ntype2id)
+
+    # Vectorize training set
+    gen_vectorized_graphs(
+                          etype2oh=etype2onehot,
+                          ntype2oh=ntype2onehot,
+                          split_files=cfg.dataset.train_files,
+                          out_dir=os.path.join(cfg.featurization.embed_edges._edge_embeds_dir, "train/"),
+                          logger=logger,
+                          cfg=cfg
+                          )
+
+    # Vectorize validation set
+    gen_vectorized_graphs(
+                          etype2oh=etype2onehot,
+                          ntype2oh=ntype2onehot,
+                          split_files=cfg.dataset.val_files,
+                          out_dir=os.path.join(cfg.featurization.embed_edges._edge_embeds_dir, "val/"),
+                          logger=logger,
+                          cfg=cfg
+                          )
+
+    # Vectorize testing set
+    gen_vectorized_graphs(
+                          etype2oh=etype2onehot,
+                          ntype2oh=ntype2onehot,
+                          split_files=cfg.dataset.test_files,
+                          out_dir=os.path.join(cfg.featurization.embed_edges._edge_embeds_dir, "test/"),
+                          logger=logger,
+                          cfg=cfg
+                          )
 
 if __name__ == '__main__':
     args = get_runtime_required_args()
