706c0c8dd4be04c2f9f6e672f2d8e92fafcefb4a
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-08-17 14:36:38 -0700

    update scripts with best params for CLEARSCOPE_E3 and CADETSE3

--- run_orthrus_node_detection.sh
+++ run_orthrus_node_detection.sh
@@ -1,7 +1,7 @@
 nohup sh -c "\
 ./run_serial.sh best_model THEIA_E3 && \
-./run_serial.sh best_model CADETS_E3 --detection.gnn_training.num_epochs=20  && \
-./run_serial.sh best_model CLEARSCOPE_E3 --preprocessing.build_graphs.time_window_size=1.0 --detection.evaluation.node_evaluation.kmeans_top_K=50" &
+./run_serial.sh best_model CADETS_E3 --detection.gnn_training.num_epochs=20 --detection.gnn_training.encoder.graph_attention.dropout=0.1 && \
+./run_serial.sh best_model CLEARSCOPE_E3 --preprocessing.build_graphs.time_window_size=1.0 --detection.gnn_training.node_out_dim=128" &
 nohup sh -c "\
 ./run_serial.sh best_model THEIA_E5 --detection.gnn_training.lr=0.000005 --preprocessing.build_graphs.use_all_files=False && \
 ./run_serial.sh best_model CADETS_E5 && \
