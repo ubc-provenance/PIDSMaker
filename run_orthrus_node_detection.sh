74c749bd2ea6244ce88de5d02c3e3494dc1a3169
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-08-19 08:36:01 -0700

    update best parameters script

--- run_orthrus_node_detection.sh
+++ run_orthrus_node_detection.sh
@@ -5,4 +5,4 @@ nohup sh -c "\
 nohup sh -c "\
 ./run_serial.sh best_model THEIA_E5 --detection.gnn_training.lr=0.000005 --preprocessing.build_graphs.use_all_files=False && \
 ./run_serial.sh best_model CADETS_E5 && \
-./run_serial.sh best_model CLEARSCOPE_E5 --detection.evaluation.node_evaluation.kmeans_top_K=100" &
+./run_serial.sh best_model CLEARSCOPE_E5 --detection.gnn_training.num_epochs=10 --detection.gnn_training.lr=0.0001" &
