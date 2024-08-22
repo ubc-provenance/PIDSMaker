05e4c55c29bcdaa4bec4ca2ab7abca33a5c6843b
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-08-22 06:52:17 -0700

    ablation script

---
+++ run_orthrus_ablation.sh
@@ -0,0 +1,11 @@
+nohup sh -c "\
+./run_serial.sh best_model THEIA_E3 --exp=E3_embedding --featurization.embed_nodes.used_method=hierarchical_hashing && \
+./run_serial.sh best_model THEIA_E5 --exp=E5_embedding --featurization.embed_nodes.used_method=hierarchical_hashing" &
+
+nohup sh -c "\
+./run_serial.sh best_model THEIA_E3 --exp=E3_encoding --detection.gnn_training.encoder.tgn.use_memory=True --detection.gnn_training.decoder.predict_edge_type.used_method=kairos  && \
+./run_serial.sh best_model THEIA_E5 --exp=E5_encoding --detection.gnn_training.encoder.tgn.use_memory=True --detection.gnn_training.decoder.predict_edge_type.used_method=kairos" &
+
+nohup sh -c "\
+./run_serial.sh best_model THEIA_E3 --exp=E3_clustering --detection.evaluation.node_evaluation.use_kmeans=False  && \
+./run_serial.sh best_model THEIA_E5 --exp=E5_clustering --detection.evaluation.node_evaluation.use_kmeans=False" &
