5dc8cdc6636c31e07ce4f44500012444c2c0e5c1
jiang-baoxiang <2402395668@qq.com>
2024-08-27 08:24:57 -0700

    update test.py

--- src/test.py
+++ src/test.py
@@ -1,20 +1,19 @@
 from provnet_utils import *
 from config import *
 
+from labelling import get_ground_truth
+from detection.evaluation_methods.evaluation_utils import *
+
 def main(cfg):
-    split_files = cfg.dataset.train_files
-    graph_dir = cfg.preprocessing.build_graphs._graphs_dir
-    sorted_paths = get_all_files_from_folders(graph_dir, split_files)
+    log("Get ground truth")
+    GP_nids, _, _ = get_ground_truth(cfg)
+    GPs = [str(nid) for nid in GP_nids]
+    print(f"There are {len(GPs)} malicious nodes")
 
-    test_graph_path = sorted_paths[0]
-    test_graph = torch.load(test_graph_path)
+    tw_to_malicious_nodes = compute_tw_labels(cfg)
+    for tw, mn in tw_to_malicious_nodes.items():
+        print(f"tw_to_malicious_nodes: {tw}, {mn}")
 
-    node, data = list(test_graph.nodes(data=True))[0]
-    print(f"Node 0 is {node}, its attrs are {data}")
-    print(type(node))
-
-    u, v, k, data = list(test_graph.edges(keys=True,data=True))[0]
-    print(f"edge {u} -> {v} {k}, its attrs are {data}")
 
 
 if __name__ == "__main__":
