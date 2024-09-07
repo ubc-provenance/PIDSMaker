cf4f60127123fd70b85d65d9480c30498e5eeb11
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-09-07 09:45:48 -0700

    update experiments

--- scripts/run_orthrus_node_detection.sh
+++ scripts/run_orthrus_node_detection.sh
@@ -4,4 +4,4 @@
 
 ./run.sh orthrus THEIA_E5 --detection.gnn_training.lr=0.000005
 ./run.sh orthrus CADETS_E5 --detection.gnn_training.node_out_dim=128 --preprocessing.build_graphs.time_window_size=3
-./run.sh orthrus CLEARSCOPE_E5 --detection.gnn_training.num_epochs=10 --detection.gnn_training.lr=0.0001
+./run.sh orthrus CLEARSCOPE_E5 --detection.gnn_training.num_epochs=10 --detection.gnn_training.lr=0.0001 --detection.gnn_training.encoder.graph_attention.dropout=0.25
