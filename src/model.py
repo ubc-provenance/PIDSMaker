41502c0bdb573fe9d7ac6de70230c1a0d57d699c
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-11-06 08:20:47 -0800

    draft for val_score v2

--- src/model.py
+++ src/model.py
@@ -3,6 +3,7 @@ from config import *
 import torch.nn as nn
 from encoders import TGNEncoder
 from experiments.uncertainty import activate_dropout_inference
+from sklearn.metrics import average_precision_score as ap_score
 
 
 class Model(nn.Module):
@@ -34,6 +35,9 @@ class Model(nn.Module):
         self.node_level = node_level
         self.is_running_mc_dropout = is_running_mc_dropout
         
+        self.positives = []
+        self.negatives = []
+        
     def embed(self, batch, full_data, inference=False, **kwargs):
         train_mode = not inference
         if self.node_level:
@@ -53,8 +57,42 @@ class Model(nn.Module):
                 edge_types= batch.edge_type
             )
         return h
-        
+    
     def forward(self, batch, full_data, inference=False, validation=False):
+        results = self._forward(batch, full_data, inference=inference, validation=validation)
+        loss_or_losses = results["loss"]
+        
+        if validation:
+            neg_batch, picked_idx = self._fast_negative_sampling(batch)
+            neg_losses = self._forward(batch, full_data, inference=inference, validation=validation, ignore_tgn=True)["loss"]
+            
+            if self.node_level:
+                neg_dst_nodes = batch.dst[picked_idx].unique()
+                picked_neg_losses = neg_losses[neg_dst_nodes]
+            else:
+                picked_neg_losses = neg_losses[picked_idx]
+            
+            self.positives.extend(loss_or_losses)
+            self.negatives.extend(picked_neg_losses)
+    
+        return results
+    
+    def _fast_negative_sampling(self, batch, aug_coef=0.2, **kwargs):        
+        edge_index = batch.edge_index
+        
+        candidates = edge_index.unique() # we consider both src and dst nodes in the batch as candidates to be negative
+        neg_idx = torch.randint(0, len(candidates), (len(edge_index[0]),))
+        candidates = candidates[neg_idx]
+        
+        picked_idx = torch.randint(0, len(edge_index[0]), (int(len(edge_index[0])*aug_coef),))
+        edge_index[0, picked_idx] = candidates[picked_idx]
+        
+        batch.src = edge_index[0]
+        batch.dst = edge_index[1]
+        
+        return batch, picked_idx
+
+    def _forward(self, batch, full_data, inference=False, validation=False, ignore_tgn=False):
         train_mode = not inference
         if self.node_level:
             x = batch.x
@@ -71,7 +109,8 @@ class Model(nn.Module):
                 edge_feats=batch.edge_feats if hasattr(batch, "edge_feats") else None,
                 full_data=full_data, # NOTE: warning, this object contains the full graph without TGN sampling
                 inference=inference,
-                edge_types= batch.edge_type
+                edge_types= batch.edge_type,
+                ignore_tgn=ignore_tgn,
             )
 
             num_elements = None
@@ -121,8 +160,15 @@ class Model(nn.Module):
             return results
         
     def get_val_ap(self):
-        # If multiple decoders are used, we take the average of the val scores
-        return np.mean([d.get_val_score() for d in self.decoders])
+        negatives = torch.tensor(self.negatives)
+        positives = torch.tensor(self.positives)
+        labels = torch.cat([torch.ones_like(negatives), torch.zeros_like(positives)])
+        scores = torch.cat([negatives, positives])
+        ap = ap_score(labels, scores)
+        
+        self.positives = []
+        self.negatives = []
+        return ap
 
     def to_device(self, device):
         if self.device == device:
