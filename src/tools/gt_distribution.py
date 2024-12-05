d0db3d0eafd64067fd73b77d411bf0c9074dbae7
jiang-baoxiang <2402395668@qq.com>
2024-12-05 15:09:02 -0800

    move and delete tool scripts irrelevant to pipeline

--- src/tools/gt_distribution.py
+++ src/tools/gt_distribution.py
@@ -1,5 +1,11 @@
-from unittest.mock import numerics
+import sys
+import os 
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from unittest.mock import numerics
 from config import *
 from provnet_utils import *
 import wandb
