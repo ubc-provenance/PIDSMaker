bea61a78141b427ff7e17cbe8149562ab09b9a3a
Tristan <bilot.tristan@hotmail.fr>
2024-12-09 07:52:18 -0800

    update figures

--- scripts/run_all_datasets.sh
+++ scripts/run_all_datasets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Hard-coded dataset names
-# DATASETS=("CLEARSCOPE_E3" "CLEARSCOPE_E5" "CADETS_E5")
-DATASETS=("THEIA_E3" "THEIA_E5")
+DATASETS=("CLEARSCOPE_E3" "CADETS_E5")
+# DATASETS=("THEIA_E3" "THEIA_E5")
 
 # Check if the minimum required arguments are provided
 if [ "$#" -lt 2 ]; then
