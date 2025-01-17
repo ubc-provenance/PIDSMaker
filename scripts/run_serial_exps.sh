0234fb95bf0d930b7e7b8b8125924f5306fa9b20
jiang-baoxiang <2402395668@qq.com>
2025-01-16 18:29:25 -0800

    modify script

--- scripts/run_serial_exps.sh
+++ scripts/run_serial_exps.sh
@@ -1,13 +1,22 @@
-nohup sh -c "\
-./run_serial.sh mimicry THEIA_E3 --wandb --preprocessing.build_graphs.mimicry_edge_num=500 --exp=new_gt_500 && \
-./run_serial.sh mimicry THEIA_E3 --wandb --preprocessing.build_graphs.mimicry_edge_num=1000 --exp=new_gt_1000 && \
-./run_serial.sh mimicry THEIA_E3 --wandb --preprocessing.build_graphs.mimicry_edge_num=1500 --exp=new_gt_1500 && \
-./run_serial.sh mimicry THEIA_E3 --wandb --preprocessing.build_graphs.mimicry_edge_num=2000 --exp=new_gt_2000 && \
-./run_serial.sh mimicry THEIA_E3 --wandb --preprocessing.build_graphs.mimicry_edge_num=2500 --exp=new_gt_2500" &
+#!/bin/bash
+
+# This script forwards all command-line arguments to the Python script
+
+# Check if any arguments were provided
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+
+# Construct the argument string to pass to the Python script
+args=""
+for arg in "$@"; do
+    args+="$arg "
+done
 
 nohup sh -c "\
-./run_serial.sh mimicry THEIA_E3 --wandb --preprocessing.build_graphs.mimicry_edge_num=3000 --exp=new_gt_3000 && \
-./run_serial.sh mimicry THEIA_E3 --wandb --preprocessing.build_graphs.mimicry_edge_num=3500 --exp=new_gt_3500 && \
-./run_serial.sh mimicry THEIA_E3 --wandb --preprocessing.build_graphs.mimicry_edge_num=4000 --exp=new_gt_4000 && \
-./run_serial.sh mimicry THEIA_E3 --wandb --preprocessing.build_graphs.mimicry_edge_num=4500 --exp=new_gt_4500 && \
-./run_serial.sh mimicry THEIA_E3 --wandb --preprocessing.build_graphs.mimicry_edge_num=5000 --exp=new_gt_5000" &
+./run_serial.sh $args --wandb --preprocessing.build_graphs.mimicry_edge_num=1000 --exp=mimicry_1000 --tags=$1,$2 && \
+./run_serial.sh $args --wandb --preprocessing.build_graphs.mimicry_edge_num=2000 --exp=mimicry_2000 --tags=$1,$2 && \
+./run_serial.sh $args --wandb --preprocessing.build_graphs.mimicry_edge_num=3000 --exp=mimicry_3000 --tags=$1,$2 && \
+./run_serial.sh $args --wandb --preprocessing.build_graphs.mimicry_edge_num=4000 --exp=mimicry_4000 --tags=$1,$2 && \
+./run_serial.sh $args --wandb --preprocessing.build_graphs.mimicry_edge_num=5000 --exp=mimicry_5000 --tags=$1,$2 " &
